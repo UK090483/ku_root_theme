6b3ddfaf9a9e849b80aaf19909087476a8f289e1
konrad ullrich <konradullrich@me.com>
2020-01-03 16:44:54 +0100

    bla

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,6 +12,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+
 	<?php jmh_post_thumbnail(); ?>
 
 	<div class="entry-content">
@@ -19,15 +20,6 @@
 		the_content();
 		?>
 	</div><!-- .entry-content -->
-	<?php
-	wp_link_pages(
-		array(
-			'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
-			'after'       => '</nav>',
-			'link_before' => '<span class="page-number">',
-			'link_after'  => '</span>',
-		)
-	)
-	?>
+
 
 </article><!-- #post-<?php the_ID(); ?> -->
